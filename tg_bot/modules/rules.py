5affa7c77a813a65373c099dc829d37ae370ffb7
levina <82658782+levina-lab@users.noreply.github.com>
2021-05-22 23:26:59 +0700

    Update rules.py

--- tg_bot/modules/rules.py
+++ tg_bot/modules/rules.py
@@ -101,7 +101,7 @@ __help__ = """
  - /clearrules: menghapus peraturan di dalam grup.
 """
 
-__mod_name__ = "📝Rules"
+__mod_name__ = "📝 Rules"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
